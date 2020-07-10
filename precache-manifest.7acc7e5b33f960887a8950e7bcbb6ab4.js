2a51efcb546d2c7d8f373331d3bd26e6c5655e7c
Greg Wegrzynowicz <gregwegrzyn88@gmail.com>
2020-07-09 21:13:35 -0500

    Updates

---
+++ precache-manifest.7acc7e5b33f960887a8950e7bcbb6ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625109156af502256aaa01ab067345b7",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "efba948957d98eba0d28",
+    "url": "/monsters-roledex/static/css/main.624f3240.chunk.css"
+  },
+  {
+    "revision": "18e3087de538d91ee632",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.2a71acd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efba948957d98eba0d28",
+    "url": "/monsters-roledex/static/js/main.b8cd18c3.chunk.js"
+  },
+  {
+    "revision": "190f18fc45d28b0800c4",
+    "url": "/monsters-roledex/static/js/runtime-main.4be1a078.js"
+  }
+]);
